fce9a1071267db16f9fff4e8d1d2279e2de4ad25
NguyenKhoa-dev <S4xlamdong@gmail.com>
2022-01-05 16:02:47 +0700

    Update Full

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function setDayNight() {
     }        
 }    
 
-//setInterval(setDayNight, 1000);
+var y = setInterval(setDayNight, 1000);
 
-setInterval(function() {
+var x = setInterval(function() {
     var now = new Date().getTime();    
     var distance = countDownDate - now;
 
@@ -30,14 +30,17 @@ setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    if (distance < 0) {
+        clearInterval(x);       
+        startFireWorks();
+        setUpText();
+    }
+    
     daysElement.innerHTML = (days < 10) ? "0" + days : days;
     hoursElement.innerHTML = (hours < 10) ? "0" + hours : hours;
     minutesElement.innerHTML = (minutes < 10) ? "0" + minutes : minutes;
     secondsElement.innerHTML = (seconds < 10) ? "0" + seconds : seconds;
 
-    if (distance < 0) {
-        clearInterval(x);            
-    }
 }, 1000);
 
 sun.onclick = function() {
@@ -50,4 +53,25 @@ sun.onclick = function() {
         audio.pause();
         playPause = false;
     }    
+}
+
+function startFireWorks() {
+    audio.pause();
+    audio = new Audio('sounds/HPNY.mp3');
+    audio.play();
+
+    const container = document.querySelector('.fireworks-container')
+    const fireworks = new Fireworks(container)
+    fireworks.start()
+    fireworks.setOptions({ delay: { min: 10, max: 15 }});
+}
+
+function setUpText() {
+    const countDownContainer = document.querySelector('.count-down-container');
+    countDownContainer.style.animation = 'animateCountDown 1s linear';
+    countDownContainer.style.opacity = '0';
+
+    const congrate = document.querySelector('h1');
+    congrate.style.display = 'block';
+    congrate.style.animation = 'animateText 1s linear';
 }
